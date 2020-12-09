bd827d547b2c54cc1fc2080294e6d9f90df3f099
南雷 <leinan@creditease.cn>
2020-12-09 14:35:12 +0800

    feat: implement `makeGraph`

---
+++ makeGraph.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+/*
+{"derivation": "语出《法华经·法师功德品》下至阿鼻地狱。”",
+ "example": "但也有少数意志薄弱的……逐步上当，终至堕入～。★《上饶集中营·炼狱杂记》",
+ "explanation": "阿鼻梵语的译音，意译为无间”，即痛苦无有间断之意。常用来比喻黑暗的社会和严酷的牢狱。又比喻无法摆脱的极其痛苦的境地。",
+  "pinyin": "ā bí dì yù",
+   "word": "阿鼻地狱",
+   "abbreviation": "abdy"}
+*/
+type IdiomItem struct {
+	Derivation   string
+	Example      string
+	Explanation  string
+	Pinyin       string
+	Word         string
+	Abbreviation string
+}
+
+type Graph struct {
+	Word string
+	ID   int
+	next []int
+}
+
+func makeGraph() []Graph {
+	var res []IdiomItem
+	raw, err := ioutil.ReadFile("./files/idiom.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(raw, &res)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var graph = make([]Graph, len(res))
+	for i := 0; i < len(res); i++ {
+		graph[i] = Graph{Word: res[i].Word, ID: i}
+	}
+	for i := 0; i < len(res); i++ {
+		var next []int
+		word1 := []rune(res[i].Word)
+		for j := 0; j < len(res); j++ {
+			word2 := []rune(res[j].Word)
+			if word2[0] == word1[len(word1)-1] && i != j {
+				next = append(next, graph[j].ID)
+			}
+		}
+		graph[i].next = next
+	}
+	return graph
+}
+
+func writeGraph(graph []Graph) {
+	data, err := json.Marshaler(graph)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func main() {
+
+}
